5dcb856a20d70fa7cfd7fee733c073d112c8eb03
rkdms <fmsl10041004@naver.com>
2025-03-05 19:42:05 +0900

    엔진 에러나는 거 수정

--- DemoGame/D2DEngine/Helper.cpp
+++ DemoGame/D2DEngine/Helper.cpp
@@ -86,17 +86,17 @@ namespace Helper
 	//	OutputDebugStringW(buffer);
 	//}
 
-	std::string WStringToString(const std::wstring& wstr)
-	{
-		std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
-		return converter.to_bytes(wstr);
-	}
-
-	std::wstring StringToWString(const std::string& str)
-	{
-		std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
-		return converter.from_bytes(str);
-	}
+	//std::string WStringToString(const std::wstring& wstr)
+	//{
+	//	std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+	//	return converter.to_bytes(wstr);
+	//}
+	//
+	//std::wstring StringToWString(const std::string& str)
+	//{
+	//	std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+	//	return converter.from_bytes(str);
+	//}
 
 
 	//std::string GetNameFromTypeName(const std::string& typeidName)
