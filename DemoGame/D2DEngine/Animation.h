c621f9becb61ecf7c14c58621bbf5dd7e7a6bda1
rkdms <fmsl10041004@naver.com>
2025-03-18 02:06:47 +0900

    애니메이션 작업중

--- DemoGame/D2DEngine/Animation.h
+++ DemoGame/D2DEngine/Animation.h
@@ -1,5 +1,25 @@
 #pragma once
-class Animation
+
+struct Frame_INFO
 {
+	D2D1_RECT_F source;		// 크기
+	D2D1_VECTOR_2F center;	// 중심
+	float duration;			// 주기
+};
+
+struct Animation_INFO
+{
+	std::string name;				// 애니메이션 이름
+	std::vector<Frame_INFO> frames;	// 프레임 모음
+};
+
+class AnimationAsset
+{
+public:
+	AnimationAsset() {};
+	~AnimationAsset() {};
+
+	std::vector<Animation_INFO> animations;
+
 };
 
